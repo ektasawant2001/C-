ab8330f820286a73cd2e82902e828e42f08bfc36
ektasawant2001 <68429412+ektasawant2001@users.noreply.github.com>
2021-06-04 12:55:01 +0530

    Add files via upload

---
+++ c++_area_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,a;
+    cout<<"Enter base and height of triangle:";
+    cin>>b>>h;
+    a=0.5*b*h;
+    cout<<"area of triangle is"<<a;
+    return 0;
+}
